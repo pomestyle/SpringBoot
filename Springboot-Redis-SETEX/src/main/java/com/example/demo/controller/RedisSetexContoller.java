cf8ca156683bac5a9169b6b760aed93e5ef7df2f
pomestyle <43631117+pomestyle@users.noreply.github.com>
2020-06-29 22:42:15 +0800

    分布式锁

--- Springboot-Redis-SETEX/src/main/java/com/example/demo/controller/RedisSetexContoller.java
+++ Springboot-Redis-SETEX/src/main/java/com/example/demo/controller/RedisSetexContoller.java
@@ -28,37 +28,6 @@ public class RedisSetexContoller {
     RedisTemplate redisTemplate;
 
 
-    @RequestMapping("setIfAbsent")
-    public String setnx() {
-        //setIfAbsent 实现分布式锁
-        return setnxVoid();
-    }
-
-    private String setnxVoid() {
-        //不存在
-        Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent("1:lock", "1");
-
-        //待超时时间的分布式锁
-        //Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent("1:lock", "1",4000, TimeUnit.SECONDS);
-        if(aBoolean){
-            redisTemplate.opsForValue().set("1:lock","1");
-            System.err.println("执行业务逻辑....");
-            redisTemplate.delete("1:lock");
-            return "执行成功！";
-        }else {
-
-            System.out.println("没拿到锁 等待自旋...");
-            try {
-                Thread.sleep(3_000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            return setnxVoid();
-        }
-    }
-
-
     @RequestMapping("test")
     public String index0() {
 
